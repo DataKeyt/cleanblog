f5686628df4305f82246e86f16f72d62ca384676
DataKeyt <retekeyt@gmail.com>
2015-10-14 11:56:24 +0300

    Lisatud include database.php faili

--- include/database.php
+++ include/database.php
@@ -5,6 +5,7 @@
  * Date: 12.10.2015
  * Time: 22:22
  */
+require_once 'config.php';
 //connect DB
 $db = mysqli_connect('127.0.0.1', 'root', '', 'cleanblog') or die(mysqli_error($db));
 mysqli_query($db, "SET NAMES 'utf8'");
